cd004df9f5dcba3b413e63e170aaf8ead2b75111
Asquardian <alexromanov5262@gmail.com>
2020-05-26 13:35:25 +0700

    Fix test

--- test/test.cpp
+++ test/test.cpp
@@ -9,31 +9,31 @@ TEST(CheckAnswer, Answer1){
 	*questionNumbers=massive();
     EXPECT_EQ(checkAnswer(0, 1), 1);
 }
-/* TEST(CheckAnswer,Answer2){
+TEST(CheckAnswer,Answer2){
 short questionNumbers1[20];
     
     *questionNumbers1 = massive();
-EXPECT_EQ(checkAnswer( 2, 3), 1 );
+EXPECT_EQ(checkAnswer( 1, 3), 1 );
 
 }
 TEST(CheckAnswer,Answer3){
 short questionNumbers3[20];
     
     *questionNumbers3 = massive();
-EXPECT_EQ(checkAnswer( 3, 1), 1 );
+EXPECT_EQ(checkAnswer( 2, 1), 1 );
 
 }
 TEST(CheckAnswer,Answer4){
 short questionNumbers4[20];
     
     *questionNumbers4 = massive();
-EXPECT_EQ(checkAnswer( 4, 3), 1 );
+EXPECT_EQ(checkAnswer( 3, 3), 1 );
 
 }
 TEST(CheckAnswer,Answer5){
 short questionNumbers[20];
     
     *questionNumbers = massive();
-EXPECT_EQ(checkAnswer( 5, 3), 1 );
+EXPECT_EQ(checkAnswer( 4, 3), 1 );
 
-}*/
+}
