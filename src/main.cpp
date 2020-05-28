7d8e0eb28e2a0d0c51dab59806acfb38e3081516
Alexey <alexromanov5262@gmail.com>
2020-05-28 16:25:05 +0700

    Fix bug

--- src/main.cpp
+++ src/main.cpp
@@ -10,38 +10,35 @@
 
 using namespace std;
 
-int main()
-{
-    readBase();
-    init();
-    short questionNumbers[20];
-    while (true) {
-        intro();
-        mainMenu();
-        srand(time(NULL));
-        *questionNumbers = massive();
-        short resultCounter = 0;
-        for (int i = 0; i < 20; ++i)
-            questionNumbers[i] = i;
-        for (int i = 0; i < 20; ++i) {
-            questionNumbers[i] = i % dataBase.size();
-            for (int j = i - 1; j >= 0; --j) {
-                if (questionNumbers[i] == questionNumbers[j]) {
-                    --i;
-                    break;
-                }
-                short resultCounter = 0;
-                short choose = 0;
-                for (short i = 0; i < 20; ++i) {
-                    showQuestion(questionNumbers[i], i + 1);
-                    choose = readKey();
-                    if (checkAnswer(i, choose))
-                        resultCounter++;
-                }
-            }
-            showResult(resultCounter);
+int main() {
+  readBase();
+  init();
+  short questionNumbers[20];
+  while (true) {
+    intro();
+    mainMenu();
+    srand(time(NULL));
+    *questionNumbers = massive();
+    short resultCounter = 0;
+    for (int i = 0; i < 20; ++i)
+      questionNumbers[i] = i;
+    for (int i = 0; i < 20; ++i) {
+      questionNumbers[i] = i % dataBase.size();
+      for (int j = i - 1; j >= 0; --j) {
+        if (questionNumbers[i] == questionNumbers[j]) {
+          --i;
+          break;
         }
-        endProgramm();
-        return 0;
-    }
+        short choose = 0;
+        for (short i = 0; i < 20; ++i) {
+          showQuestion(questionNumbers[i], i + 1);
+          choose = readKey();
+          if (checkAnswer(i, choose))
+            resultCounter++;
+        }
+        showResult(resultCounter);
+      }
+    } 
+  }
+  return 0;
 }
